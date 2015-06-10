aa27f732b18093d0f22895e7c8f91279e727baf2
Kadi Kraman <hellokadi@gmail.com>
2015-06-10 18:34:25 +0100

    Robots can traverse the map (but do not fall off the edge)

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 from game import Mars, Robot
 
 
-mars = Mars(5, 3)
+def generate_output_string(robots):
+    """
+    Generate the output string based on current state of the robots
+    :param robots: array of Robots
+    :return: output string
+    """
+    output = ''
+    for robot in robots:
+        output += "{0} {1} {2}\n".format(robot.current_x, robot.current_y, robot.current_direction)
+    return output
 
-robot1 = Robot(1, 1, 'E', 'RFRFRFRF')
-robot2 = Robot(3, 2, 'N', 'FRRFLLFFRRFLL')
-robot3 = Robot(0, 3, 'S', 'LLFFFLFLFL')
 
-robots = [robot1, robot2, robot3]
+def play(robots):
+    """
+    Play! The game logic lives here.
+    Robots traverse Mars in order. If the fall of the map, the tile they were standing on becomes 'scented'
+    so that future robots will not fall off that tile (the instruction will be ignored)
+    :param robots: array of Robots
+    :return: result string
+    """
+    for robot in robots:
+        for instruction in robot.instructions:
+            if instruction in ['L', 'R']:
+                robot.turn(instruction)
+            elif instruction in ['F']:
+                robot.move_forward()
+    return generate_output_string(robots)
+
+def main():
+    mars = Mars(5, 3)
+    robot1 = Robot(1, 1, 'E', 'RFRFRFRF')
+    robot2 = Robot(3, 2, 'N', 'FRRFLLFFRRFLL')
+    robot3 = Robot(0, 3, 'S', 'LLFFFLFLFL')
+    robots = [robot1, robot2, robot3]
+
+    print(play(mars, robots))
+
+
+if __name__ == '__main__':
+    main()
